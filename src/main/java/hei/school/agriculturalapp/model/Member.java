e5403405f0e7f228c8f7b3906d55279854f3428d
nassaigael <hei.nassai@gmail.com>
2026-04-21 21:19:42 +0300

    refactor(member): change untils methode using Lombok

--- src/main/java/hei/school/agriculturalapp/model/Member.java
+++ src/main/java/hei/school/agriculturalapp/model/Member.java
@@ -1,10 +1,19 @@
 package hei.school.agriculturalapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Member {
     private Integer id;
     private String firstName;
@@ -19,56 +28,6 @@ public class Member {
     private List<Integer> refereeIds;
 
 
-    public Member(Integer id, String firstName, String lastName, LocalDate birthDate, String gender, String phoneNumber, boolean registrationFeePaid, boolean membershipDuesPaid, String admissionStatus, Integer collectivityId, List<Integer> refereeIds) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.registrationFeePaid = registrationFeePaid;
-        this.membershipDuesPaid = membershipDuesPaid;
-        this.admissionStatus = admissionStatus;
-        this.collectivityId = collectivityId;
-        this.refereeIds = refereeIds;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -79,58 +38,6 @@ public class Member {
     @Override
     public int hashCode() {
         return Objects.hash(id, firstName, lastName, birthDate, gender, phoneNumber, registrationFeePaid, membershipDuesPaid, admissionStatus, collectivityId, refereeIds);
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public boolean isRegistrationFeePaid() {
-        return registrationFeePaid;
-    }
-
-    public void setRegistrationFeePaid(boolean registrationFeePaid) {
-        this.registrationFeePaid = registrationFeePaid;
-    }
-
-    public boolean isMembershipDuesPaid() {
-        return membershipDuesPaid;
-    }
-
-    public void setMembershipDuesPaid(boolean membershipDuesPaid) {
-        this.membershipDuesPaid = membershipDuesPaid;
-    }
-
-    public String getAdmissionStatus() {
-        return admissionStatus;
-    }
-
-    public void setAdmissionStatus(String admissionStatus) {
-        this.admissionStatus = admissionStatus;
-    }
-
-    public Integer getCollectivityId() {
-        return collectivityId;
-    }
-
-    public void setCollectivityId(Integer collectivityId) {
-        this.collectivityId = collectivityId;
-    }
-
-    public List<Integer> getRefereeIds() {
-        return refereeIds;
-    }
-
-    public void setRefereeIds(List<Integer> refereeIds) {
-        this.refereeIds = refereeIds;
     }
 
 
