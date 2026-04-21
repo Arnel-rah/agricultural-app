f34fea7d3765d742ab8c3703e0c4204b4d2b7fe1
nassaigael <hei.nassai@gmail.com>
2026-04-21 16:14:30 +0300

    refactor(database-config): add bean annotation on datasource method

--- src/main/java/hei/school/agriculturalapp/config/DatabaseConfig.java
+++ src/main/java/hei/school/agriculturalapp/config/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package hei.school.agriculturalapp.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -12,6 +13,7 @@ import java.sql.DriverManager;
 public class DatabaseConfig {
     Dotenv  dotenv = Dotenv.load();
 
+    @Bean
     public Connection dataSource() {
        try {
             String url = dotenv.get("DB_URL");
