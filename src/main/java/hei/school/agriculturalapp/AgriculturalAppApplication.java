6144919756c8df7183d889b37ad7b70b3fa4efc2
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-04-21 15:28:06 +0300

    feat: init project spring with dependancies

---
+++ src/main/java/hei/school/agriculturalapp/AgriculturalAppApplication.java
@@ -0,0 +1,13 @@
+package hei.school.agriculturalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/school/agriculturalapp/AgriculturalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.agriculturalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
