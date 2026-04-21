ffd1120e57ea347f6e4479b2631741151791f975
nassaigael <hei.nassai@gmail.com>
2026-04-21 21:30:29 +0300

    refactor(database-config): remove unused import

--- src/main/java/hei/school/agriculturalapp/config/DatabaseConfig.java
+++ src/main/java/hei/school/agriculturalapp/config/DatabaseConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
