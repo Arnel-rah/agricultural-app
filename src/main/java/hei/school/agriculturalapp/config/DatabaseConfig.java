023ff020a17e2eb2108e61289cc17fbeeb47c412
nassaigael <hei.nassai@gmail.com>
2026-04-21 15:52:38 +0300

    feat(global): add .gitkeeping to push empty folder

---
+++ src/main/java/hei/school/agriculturalapp/config/DatabaseConfig.java
@@ -0,0 +1,14 @@
+package hei.school.agriculturalapp.config;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public DataSource dataSource() {
+    }
+}
