12058dee6be74e525fd8c533a6e6db2abb9c1fb8
nassaigael <hei.nassai@gmail.com>
2026-04-22 18:59:01 +0300

    feat(create-membership-fee): create new dto

---
+++ src/main/java/hei/school/agriculturalapp/dto/CreateMembershipFee.java
@@ -0,0 +1,15 @@
+package hei.school.agriculturalapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class CreateMembershipFee {
+    private LocalDate eligibleFrom;
+    private String frequency;
+    private Double amount;
+    private String label;
+}
