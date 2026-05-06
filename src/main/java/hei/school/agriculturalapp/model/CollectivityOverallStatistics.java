93fd5224f0d9641da2b888698220e2032a7a15b0
nassaigael <hei.nassai@gmail.com>
2026-05-06 09:38:00 +0300

    feat(model): add CollectivityOverallStatistics model for federation statistics

--- src/main/java/hei/school/agriculturalapp/model/CollectivityOverallStatistics.java
+++ src/main/java/hei/school/agriculturalapp/model/CollectivityOverallStatistics.java
@@ -1,4 +1,16 @@
 package hei.school.agriculturalapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CollectivityOverallStatistics {
+    private Collectivity collectivityInformation;
+    private Integer newMembersNumber;
+    private Double overallMemberCurrentDuePercentage;
 }
