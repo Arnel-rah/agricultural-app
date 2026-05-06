4997b0ca8064d7d100840200afd30de040f158e3
nassaigael <hei.nassai@gmail.com>
2026-05-06 09:36:00 +0300

    feat(model): add CollectivityLocalStatistics model for per-member statistics

--- src/main/java/hei/school/agriculturalapp/model/CollectivityLocalStatistics.java
+++ src/main/java/hei/school/agriculturalapp/model/CollectivityLocalStatistics.java
@@ -1,4 +1,16 @@
 package hei.school.agriculturalapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class CollectivityLocalStatistics {
+    private MemberDescription memberDescription;
+    private Double earnedAmount;
+    private Double unpaidAmount;
 }
