fc3048cfb03ccf7cd6bb84a5c71e6418ecba7486
nassaigael <hei.nassai@gmail.com>
2026-05-06 09:32:00 +0300

    feat(model): add Attendance entity for member activity tracking

--- src/main/java/hei/school/agriculturalapp/model/Attendance.java
+++ src/main/java/hei/school/agriculturalapp/model/Attendance.java
@@ -1,4 +1,21 @@
 package hei.school.agriculturalapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Attendance {
+    private String id;
+    private String activityId;
+    private String memberId;
+    private String attendanceStatus;
+    private String memberFirstName;
+    private String memberLastName;
+    private String memberEmail;
+    private String memberOccupation;
 }
