dc1040946344bcac2df2ab626be764fd3dbc9ca8
nassaigael <hei.nassai@gmail.com>
2026-05-06 09:44:00 +0300

    feat(dto): add RecurrenceRule DTO for monthly activity recurrence

--- src/main/java/hei/school/agriculturalapp/dto/RecurrenceRule.java
+++ src/main/java/hei/school/agriculturalapp/dto/RecurrenceRule.java
@@ -1,4 +1,15 @@
 package hei.school.agriculturalapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RecurrenceRule {
+    private Integer weekOrdinal;
+    private String dayOfWeek;
 }
